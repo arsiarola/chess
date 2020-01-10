9dd551a08e65019ffde56645b67727373c9e92a2
arsiarola <1andre234@gmail.com>
2020-01-11 00:16:07 +0200

    main improved

--- main.cpp
+++ main.cpp
@@ -1,31 +1,42 @@
+#include "main.h"
+#include "tools.h"
 #include "board.h"
 #include <iostream>
 
 using std::cout;
 
 int main() {
-        Board board;
-    while (true) {
-        int option = 0;
-        cout << "Enter one of the options below\n"
-            "1. Start new game\n"
-            "2. Load existing game\n"
-            "3. Quit\n";
+    
+    Board board;
+    int option = 0;
+    print_options();
+    while (!(std::cin >> option)) {
+	cout << "Incorrect value, please enter one of the options below\n";
+	print_options();
+	std::cin.clear();
+	std::cin.ignore(256, '\n');
 
-        std::cin >> option;
-        cout << option;
+    }
 
-        switch (option) {
-        case PLAY:
-            while (!board.is_game_over()) {
-                board.players_turn();
-            }
+    switch (option) {
+    case PLAY:
+	while (!board.is_game_over()) {
+	    board.players_turn();
+	}
 
-        case QUIT:
-            return 0;
-        }
+    case QUIT:
+	return 0;
     }
 
     cout << "Game ended winner is " << board.get_winner_string() << "\n";
     return 0;
+}
+
+
+void print_options() {
+    cout << "Enter one of the options below\n"
+	 << PLAY << ". Start new game\n"
+	 << QUIT << ". Quit\n";
+    
+
 }

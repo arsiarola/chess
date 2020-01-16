fa735d2c89d0e36b1a464aed1fa4e8c7fa272d3e
arsiarola <1andre234@gmail.com>
2020-01-17 01:53:49 +0200

    main vihdoin oikeasti logiikka kunnos

--- main.cpp
+++ main.cpp
@@ -9,34 +9,39 @@ int main() {
     
     Board board;
     int option = 0;
+    bool correct = false;
     print_options();
-    while (!(std::cin >> option)) {
-	cout << "Incorrect value, please enter one of the options below\n";
-	print_options();
-	std::cin.clear();
-	std::cin.ignore(256, '\n');
-
-    }
-
-    switch (option) {
-    case PLAY:
-	while (!board.is_game_over()) {
-	    board.players_turn();
+    while (!correct) {
+	while (!(std::cin >> option)) {
+	    cout << "the input has to be a number!\n";
 	}
 
-    case QUIT:
-	return 0;
-    }
+	switch (option) {
+	case PLAY:
+	    correct = true;
+	    while (!board.is_game_over()) {
+		board.players_turn();
+	    }
+	    break;
 
+	case QUIT:
+	    correct = true;
+	    return 0;
+   
+	default:
+	    cout << "Incorrect value, please enter one of the options below\n";
+	    print_options();
+	    std::cin.clear();
+	    std::cin.ignore(256, '\n');
+	    break; 
+	}
+    }
     cout << "Game ended winner is " << board.get_winner_string() << "\n";
     return 0;
 }
-
 
 void print_options() {
     cout << "Enter one of the options below\n"
 	 << PLAY << ". Start new game\n"
 	 << QUIT << ". Quit\n";
-    
-
 }
